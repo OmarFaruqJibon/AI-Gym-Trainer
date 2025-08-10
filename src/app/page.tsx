9d8003ea9c9ba3a5d7652649b653e2ee7da706bd
OmarFaruqJibon <omarfarukjibon418@gmail.com>
2025-08-11 00:04:04 +0600

    authentication setup using Clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+"use-client";
+
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+} from "@clerk/nextjs";
 import React from "react";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className="flex justify-center items-center gap-7">
+      Homepage
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+    </div>
+  );
 };
 
 export default Homepage;
